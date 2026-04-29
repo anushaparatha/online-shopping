d1f25ad4ab6b861d9ce58f61be59292084cf1d8b
pavi <pavithra0161104@gmail.com>
2026-04-29 13:41:34 +0530

    updated message

--- shop.js
+++ shop.js
@@ -1,4 +1,6 @@
 console.log("Chennai Fashion is a modern online shopping platform");
 console.log("Mens wear is Availables");
 console.log("mens t-shirt");
-console.log("mens shirt");    
+console.log("mens shirt");   
+console.log("jeans");
+console.log("formal pant");
